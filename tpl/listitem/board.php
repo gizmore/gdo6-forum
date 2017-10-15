20afb9f6f623e889d8c691ef8cdfc9b0e20c5885
gizmore <gizmore@wechall.net>
2017-10-15 22:46:51 +0200

    6.06_layout

--- tpl/listitem/board.php
+++ tpl/listitem/board.php
@@ -10,15 +10,15 @@ $subscribeClass = $subscribed ? 'gdo-forum gdo-forum-subscribed' : 'gdo-forum';
 $readClass = $board->hasUnreadPosts($user) ? 'gdo-forum-unread' : 'gdo-forum-read';
 $href = href('Forum', 'Boards', '&board='.$bid);
 ?>
-<li class="<?=$readClass;?> <?=$subscribeClass;?>">
-  <a class="gdo-list-content" href="<?= $href; ?>">
-    <span class="b"><?= $board->displayName(); ?></span>
-    <span class="b"><?= $board->displayDescription(); ?></span>
-    <span class="b"><?= t('board_stats', [$board->getThreadCount(), $board->getPostCount()]); ?></span>
+<div class="gdt-list-item <?=$readClass;?> <?=$subscribeClass;?>">
+  <a class="gdt-content" href="<?= $href; ?>">
+    <span class="line1"><?= $board->displayName(); ?></span>
+    <span class="line2"><?= $board->displayDescription(); ?></span>
+    <span class="line3"><?= t('board_stats', [$board->getThreadCount(), $board->getPostCount()]); ?></span>
   </a>
-  <span class="gdo-list-actions">
+  <span class="gdt-actions">
     <?php $href = $subscribed ? href('Forum', 'Unsubscribe', '&board='.$bid) : href('Forum', 'Subscribe', '&board='.$bid)?>
     <?= GDT_IconButton::make()->href($href)->icon('email')->render(); ?>
     <?= GDT_Icon::iconS('arrow_right'); ?>
   </span>
-</li>
+</div>
