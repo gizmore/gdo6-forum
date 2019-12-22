50e69e75f150e35138a717f190cc00788bfe5d78
gizmore <gizmore@wechall.net>
2019-12-22 18:58:11 +0100

    6.10-r1042_it

---
+++ lang/forum_de.php
@@ -0,0 +1,90 @@
+<?php
+return array(
+'link_forum' => '%s Posts',
+'gdo_forumpost' => 'Post',
+'gdo_forumboard' => 'Board',
+'board' => 'Board',
+'parent' => 'Parent',
+'board_stats' => '%s Threads, %s Posts',
+'btn_reply' => 'Reply',
+'btn_quote' => 'Quote',
+'btn_create_thread' => 'Start new thread',
+'btn_subscribe' => 'Subscribe',
+'btn_unsubscribe' => 'Unsubscribe',
+'btn_view_first_post' => 'First post',
+'btn_view_last_post' => 'Last post',
+'list_title_board_threads' => 'There are %s threads in this board',
+'list_title_thread_posts' => '%s – %s Posts',
+'thread_postcount' => '%s Posts',
+'posted_at' => 'Posted at %s',
+'li_thread_created' => 'By %s at %s',
+'li_thread_replies' => '%s replies. Last by %s at %s',
+'li_thread_no_replies' => '- No replies yet -',
+'hidden_post_level' => 'You need a userlevel of %s to read this post.',
+'forum_list_latest_threads' => 'Latest Forum Activity',
+'last_post' => 'Last post from %s by %s',
+'list_threads' => '%s Threads in %s',
+'board_allow_threads' => 'Allow threads?',
+#######################################################################
+'cfg_forum_root' => 'Forum board root',
+'tt_cfg_forum_root' => 'This board is automatically created during install.',
+'cfg_forum_guest_posts' => 'Allow guest posts?',
+'cfg_forum_attachments' => 'Allow attachments?',
+'cfg_forum_attachment_level' => 'Userlevel required for attachments',
+'cfg_forum_post_level' => 'Userlevel required to post',
+'cfg_forum_latest_post_date' => 'Latest forum post date',
+'cfg_tt_forum_latest_post_date' => 'This timestamp is not configurable',
+'cfg_forum_mail_sent_for_post' => 'Latest PostId for mail queue.',
+'cfg_tt_forum_mail_sent_for_post' => 'This internal setting is not configurable.',
+'cfg_forum_num_latest' => 'Number of latest threads shown in root board',
+#######################################################################
+'msg_subscribed' => 'You have subscribed via mail.',
+'msg_unsubscribed' => 'You have not subscribed via mail.',
+#######################################################################
+'no_parent' => 'No parent selected',
+'board_allow_threads' => 'Allow threads for this board',
+#######################################################################
+'ft_forum_createthread' => 'Start new Thread',
+'msg_thread_created' => 'Your thread has been created successfully.',
+#######################################################################
+'ft_forum_editthread' => 'Edit Thread',
+'msg_thread_moved' => 'Your thread has been moved to another board.',
+'msg_thread_edited' => 'Your thread has been edited successfully.',
+#######################################################################
+'list_forum_search' => 'Search for %s',
+#######################################################################
+'signature' => 'Your signature',
+'forum_subscription_mode' => 'Forum E-Mail Subscription Mode',
+'tt_forum_subscription_mode' => 'Also check account settings for E-Mail options',
+'forum_posts' => 'Your total posts',
+'forum_threads' => 'Your total created threads',
+'forum_readmark' => 'ReadMarker to speedup Unread Engine',
+#######################################################################
+'enum_fsub_none' => 'Only Manually per thread/board',
+'enum_fsub_own' => '+ Auto to threads where i posted',
+'enum_fsub_all' => 'Automatically subscribe the whole forum',
+#######################################################################
+'mail_subj_forum_post' => '[%s] Post in %s',
+'mail_body_forum_post' => '
+Dear %s,
+
+There has been made a new forum post on %s in one of your subscribed threads.
+
+Thread: %s
+From: %s
+----------------------------------------------------------
+
+%s
+
+----------------------------------------------------------
+
+If you do not like to get any further forum notifications,
+you can click on the link below.
+
+%s
+
+Kind Regards
+The %2$s Team',
+#######################################################################
+'div_user_textual_settings' => 'Textual Settings',
+);
