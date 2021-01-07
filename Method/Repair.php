7b8a2763ebf6274baaddc7436f3dda6c90dffdc1
gizmore <gizmore@wechall.net>
2021-01-07 20:20:06 +0100

    6.10-r9432_tbs

--- Method/Repair.php
+++ Method/Repair.php
@@ -5,6 +5,7 @@ use GDO\Form\GDT_Form;
 use GDO\Form\MethodForm;
 use GDO\Form\GDT_Submit;
 use GDO\Form\GDT_AntiCSRF;
+use GDO\Forum\GDO_ForumThread;
 
 /**
  * Repair values like likes, lastposter, lastpostdate, etc.
@@ -23,7 +24,34 @@ final class Repair extends MethodForm
 
     public function formValidated(GDT_Form $form)
     {
-        
+        $this->repair();
+        return parent::formValidated($form);
+    }
+    
+    public function repair()
+    {
+        $this->repairLastPoster();
+        $this->repairPostCount();
+    }
+    
+    private function repairLastPoster()
+    {
+        foreach (GDO_ForumThread::table()->all() as $thread)
+        {
+            $post = $thread->getLastPost();
+            if (!$post)
+            {
+                echo "break!";
+            }
+            $thread->saveVars([
+                'thread_lastposter' => $post->getCreatorID(),
+                'thread_lastposted' => $post->getCreated(),
+            ]);
+        }
+    }
+    
+    private function repairPostCount()
+    {
         
     }
     
